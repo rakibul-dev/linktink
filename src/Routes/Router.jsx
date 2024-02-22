4cd600c78a87c4c653f865a35e2071566f58367c
rakibul-dev <mdrakibul.dev@gmail.com>
2024-02-22 20:31:34 +0600

    feat: add login page

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,5 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../Pages/HomePage";
 
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+// Error pages
+import PageNotFound from "../Pages/Error/PageNotFound";
+import LoginPage from "../Pages/Auth/LoginPage";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "login", element: <LoginPage /> },
+  { path: "*", element: <PageNotFound /> },
+]);
 export default router;
