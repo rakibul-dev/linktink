fdb1282f79ce69cec1383b8b1bfdcdd8a6171c56
rakibul-dev <mdrakibul.dev@gmail.com>
2024-02-23 13:07:25 +0600

    refactor: add set userInfo reducer

--- src/Redux/slices/userSlice.js
+++ src/Redux/slices/userSlice.js
@@ -1,30 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  value: 0,
+  userInfo: {},
 };
 
-export const counterSlice = createSlice({
-  name: "counter",
+export const userSlice = createSlice({
+  name: "user",
   initialState,
   reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1;
-    },
-    decrement: (state) => {
-      state.value -= 1;
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
+    setUserInfo: (state, action) => {
+      state.userInfo = action.payload;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { setUserInfo } = userSlice.actions;
 
-export default counterSlice.reducer;
+export default userSlice.reducer;
