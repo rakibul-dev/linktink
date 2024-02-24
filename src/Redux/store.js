84ed1dc410d42a4968bf1230870c1999076d0ec1
rakibul-dev <mdrakibul.dev@gmail.com>
2024-02-24 07:39:39 +0600

    refactor: project slice added

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -3,9 +3,11 @@ import counterSlice from "./slices/userSlice";
 
 import logger from "redux-logger";
 import userSlice from "./slices/userSlice";
+import projectSlice from "./slices/projectSlice";
 export const store = configureStore({
   reducer: {
     user: userSlice,
+    projects: projectSlice,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
