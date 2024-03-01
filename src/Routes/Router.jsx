03f075234509646790da1cb16815df19ee2d438f
rakibul-dev <mdrakibul.dev@gmail.com>
2024-03-01 11:26:24 +0600

    refactor: add new routes

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,4 +1,5 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import Cookies from "js-cookie";
 import HomePage from "../Pages/HomePage";
 
 // Error pages
@@ -9,16 +10,30 @@ import LoginPage from "../Pages/Auth/LoginPage";
 import UserDashboard from "../Layouts/UserDashboard";
 import ProjectListing from "../Pages/UserDashboard/ProjectListing";
 import ProjectAnalytics from "../Pages/UserDashboard/ProjectAnalytics";
+import LinkListingPage from "../Pages/UserDashboard/LinkListingPage";
+import ShortlinkAnalytics from "../Pages/UserDashboard/ShortlinkAnalytics";
+
+const user = Cookies.get("user");
 
 const router = createBrowserRouter([
   {
     path: "/user/dashboard",
+    element: (
+      <Navigate to={user ? "/user/dashboard/projects" : "/login"} replace />
+    ),
+  },
+  {
+    path: "/user/dashboard",
     element: <UserDashboard />,
+
     children: [
       { path: "projects", element: <ProjectListing /> },
+      { path: "projects/:id", element: <LinkListingPage /> },
       { path: "projects/analytics/:id", element: <ProjectAnalytics /> },
+      { path: "shortlinks/analytics/:id", element: <ShortlinkAnalytics /> },
     ],
   },
+
   { path: "/", element: <HomePage /> },
   { path: "login", element: <LoginPage /> },
   { path: "*", element: <PageNotFound /> },
