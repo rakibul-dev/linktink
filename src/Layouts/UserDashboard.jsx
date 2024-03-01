40e8d031db75b7e761016730aaf65d5eb22f137d
rakibul-dev <mdrakibul.dev@gmail.com>
2024-03-01 11:33:24 +0600

    feat: add new user dashboard page

--- src/Layouts/UserDashboard.jsx
+++ src/Layouts/UserDashboard.jsx
@@ -1,12 +1,21 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, Navigate } from "react-router-dom";
 import Header from "../Components/Header/Header";
 import { Flex } from "antd";
+import Cookies from "js-cookie";
 const UserDashboard = () => {
+  const user = Cookies.get("user");
+
   return (
     <div>
-      <Header />
-      <Outlet />
+      {user ? (
+        <>
+          <Header />
+          <Outlet />
+        </>
+      ) : (
+        <Navigate to="/login" />
+      )}
     </div>
   );
 };
