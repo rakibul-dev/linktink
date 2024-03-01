566de7b47f3edebf2db58b0ed2954db342aa68d0
rakibul-dev <mdrakibul.dev@gmail.com>
2024-03-01 11:28:46 +0600

    feat: add new storeslice

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -4,10 +4,12 @@ import counterSlice from "./slices/userSlice";
 import logger from "redux-logger";
 import userSlice from "./slices/userSlice";
 import projectSlice from "./slices/projectSlice";
+import shortlinkSlice from "./slices/shortlinkSlice";
 export const store = configureStore({
   reducer: {
     user: userSlice,
     projects: projectSlice,
+    shortlinks: shortlinkSlice,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
