8b244c9c7ec50df7d6ae839222806990abf2f2a2
rakibul-dev <mdrakibul.dev@gmail.com>
2024-03-01 11:30:49 +0600

    feat: add new project analytics page

--- src/Pages/UserDashboard/ProjectAnalytics.jsx
+++ src/Pages/UserDashboard/ProjectAnalytics.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Col, Row, Flex, Card, Segmented, Tabs, Typography } from "antd";
 
 const { Title } = Typography;
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 import {
   BarChart,
@@ -13,238 +15,27 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
+import { getProjectAnalytics } from "../../Redux/slices/projectSlice";
 const ProjectAnalytics = () => {
+  const dispatch = useDispatch();
+  const {
+    clicks,
+    cities,
+    countries,
+    devices,
+    operating_systems,
+    browsers,
+    clicksByDate,
+  } = useSelector((state) => state.projects.analytics);
+  const { analytics } = useSelector((state) => state.projects);
+  const { id } = useParams();
+
   const [location, setLocation] = React.useState("Country");
   const [devicePlatform, setDevicePlatform] = React.useState("Device");
 
-  const dataArr = [
-    {
-      date: "2024-02-19T08:06:27.295Z",
-      count: 15,
-    },
-    {
-      date: "2024-02-19T08:06:49.663Z",
-      count: 8,
-    },
-  ];
-
-  let labels = [];
-
-  dataArr.map((item) => labels.push(item.date));
-  //   const labels = [
-  //     "January",
-  //     "February",
-  //     "March",
-  //     "April",
-  //     "May",
-  //     "June",
-  //     "July",
-  //   ];
-
-  const formatedData = () => {
-    let datasetArr = [];
-    for (let item of dataArr) {
-      //   console.log(item);
-      let data = {};
-      const date = item.date;
-
-      data[date] = item.count;
-
-      //   console.log(data);
-      datasetArr.push({
-        label: item.date,
-        data,
-        // data: { count: item.count },
-        //   date: new Date("2024-02-19T08:06:27.295Z"),
-
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-      });
-    }
-    // console.log({ datasetArr });
-    return datasetArr;
-  };
-  const dataset = formatedData();
-  //   const data = {
-  //     labels,
-  //     datasets: dataset,
-  //     // datasets: [
-  //     //   {
-  //     //     label: "Count",
-  //     //     data: { January: 10 },
-  //     //     //   date: new Date("2024-02-19T08:06:27.295Z"),
-
-  //     //     backgroundColor: "rgba(255, 99, 132, 0.5)",
-  //     //   },
-  //     //   //   {
-  //     //   //     label: "Dataset 1",
-  //     //   //     data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-  //     //   //     backgroundColor: "rgba(255, 99, 132, 0.5)",
-  //     //   //   },
-  //     //   //   {
-  //     //   //     label: "Dataset 2",
-  //     //   //     data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-  //     //   //     backgroundColor: "rgba(53, 162, 235, 0.5)",
-  //     //   //   },
-  //     // ],
-  //   };
-
-  const data = [
-    {
-      name: "Page A",
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    // {
-    //   name: "Page B",
-    //   uv: 3000,
-    //   pv: 1398,
-    //   amt: 2210,
-    // },
-    // {
-    //   name: "Page C",
-    //   uv: 2000,
-    //   pv: 9800,
-    //   amt: 2290,
-    // },
-
-    {
-      date: "2024-02-19T08:06:27.295Z",
-      count: 15,
-    },
-    {
-      date: "2024-02-19T08:06:49.663Z",
-      count: 8,
-    },
-    {
-      date: "2024-02-19T08:06:27.295Z",
-      count: 15,
-    },
-    {
-      date: "2024-02-19T08:06:49.663Z",
-      count: 8,
-    },
-    {
-      date: "2024-02-19T08:06:27.295Z",
-      count: 15,
-    },
-    {
-      date: "2024-02-19T08:06:49.663Z",
-      count: 8,
-    },
-    {
-      date: "2024-02-19T08:06:27.295Z",
-      count: 15,
-    },
-    {
-      date: "2024-02-19T08:06:49.663Z",
-      count: 8,
-    },
-    {
-      date: "2024-02-19T08:06:27.295Z",
-      count: 15,
-    },
-    {
-      date: "2024-02-19T08:06:49.663Z",
-      count: 8,
-    },
-    {
-      date: "2024-02-19T08:06:27.295Z",
-      count: 15,
-    },
-    {
-      date: "2024-02-19T08:06:49.663Z",
-      count: 8,
-    },
-    {
-      date: "2024-02-19T08:06:27.295Z",
-      count: 15,
-    },
-    {
-      date: "2024-02-19T08:06:49.663Z",
-      count: 8,
-    },
-    {
-      date: "2024-02-19T08:06:27.295Z",
-      count: 15,
-    },
-    {
-      date: "2024-02-19T08:06:49.663Z",
-      count: 8,
-    },
-    {
-      date: "2024-02-19T08:06:27.295Z",
-      count: 15,
-    },
-    {
-      date: "2024-02-19T08:06:49.663Z",
-      count: 8,
-    },
-    {
-      date: "2024-02-19T08:06:27.295Z",
-      count: 15,
-    },
-    {
-      date: "2024-02-19T08:06:49.663Z",
-      count: 8,
-    },
-    {
-      date: "2024-02-19T08:06:27.295Z",
-      count: 15,
-    },
-    {
-      date: "2024-02-19T08:06:49.663Z",
-      count: 8,
-    },
-    {
-      date: "2024-02-19T08:06:27.295Z",
-      count: 15,
-    },
-    {
-      date: "2024-02-19T08:06:49.663Z",
-      count: 8,
-    },
-    {
-      date: "2024-02-19T08:06:27.295Z",
-      count: 15,
-    },
-    {
-      date: "2024-02-19T08:06:49.663Z",
-      count: 8,
-    },
-    {
-      date: "2024-02-19T08:06:27.295Z",
-      count: 15,
-    },
-    {
-      date: "2024-02-19T08:06:49.663Z",
-      count: 8,
-    },
-    {
-      date: "2024-02-19T08:06:27.295Z",
-      count: 15,
-    },
-    {
-      date: "2024-02-19T08:06:49.663Z",
-      count: 8,
-    },
-    {
-      date: "2024-02-19T08:06:27.295Z",
-      count: 15,
-    },
-    {
-      date: "2024-02-19T08:06:49.663Z",
-      count: 8,
-    },
-    {
-      date: "2024-02-19T08:06:27.295Z",
-      count: 15,
-    },
-    {
-      date: "2024-02-19T08:06:49.663Z",
-      count: 8,
-    },
-  ];
+  useEffect(() => {
+    dispatch(getProjectAnalytics({ id }));
+  }, []);
 
   const locations = () => {
     if (location === "Country") {
@@ -256,7 +47,7 @@ const ProjectAnalytics = () => {
     }
   };
 
-  const devices = () => {
+  const devicesChart = () => {
     if (devicePlatform === "Device") {
       return (
         <>
@@ -265,7 +56,7 @@ const ProjectAnalytics = () => {
               //   width={700}
               style={{ width: "100%" }}
               // height={300}
-              data={data}
+              data={devices}
               margin={{
                 top: 5,
                 right: 30,
@@ -277,7 +68,7 @@ const ProjectAnalytics = () => {
               {/* <CartesianGrid strokeDasharray="3 3" /> */}
               <XAxis type="number" />
               <YAxis
-                dataKey="date"
+                dataKey="device"
                 //   yAxisId={0}
                 //   dataKey={xKey}
                 type="category"
@@ -287,7 +78,150 @@ const ProjectAnalytics = () => {
               <Tooltip />
               <Legend />
               {/* <Bar dataKey="pv" fill="#8884d8" /> */}
-              <Bar dataKey="count" fill="#3aaf61" />
+              <Bar dataKey="count" fill="#f97316" />
+            </BarChart>
+          </ResponsiveContainer>
+        </>
+      );
+    } else if (devicePlatform === "Browser") {
+      return (
+        <>
+          <ResponsiveContainer width="100%" height={400}>
+            <BarChart
+              //   width={700}
+              style={{ width: "100%" }}
+              // height={300}
+              data={browsers}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+              layout="vertical"
+            >
+              {/* <CartesianGrid strokeDasharray="3 3" /> */}
+              <XAxis type="number" />
+              <YAxis
+                dataKey="browser"
+                //   yAxisId={0}
+                //   dataKey={xKey}
+                type="category"
+                axisLine={false}
+                tickLine={false}
+              />
+              <Tooltip />
+              <Legend />
+              {/* <Bar dataKey="pv" fill="#8884d8" /> */}
+              <Bar dataKey="count" fill="#f97316" />
+            </BarChart>
+          </ResponsiveContainer>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <ResponsiveContainer width="100%" height={400}>
+            <BarChart
+              //   width={700}
+              style={{ width: "100%" }}
+              // height={300}
+              data={operating_systems}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+              layout="vertical"
+            >
+              {/* <CartesianGrid strokeDasharray="3 3" /> */}
+              <XAxis type="number" />
+              <YAxis
+                dataKey="os"
+                //   yAxisId={0}
+                //   dataKey={xKey}
+                type="category"
+                axisLine={false}
+                tickLine={false}
+              />
+              <Tooltip />
+              <Legend />
+              {/* <Bar dataKey="pv" fill="#8884d8" /> */}
+              <Bar dataKey="count" fill="#f97316" />
+            </BarChart>
+          </ResponsiveContainer>
+        </>
+      );
+    }
+  };
+  const locationsChart = () => {
+    if (location === "Country") {
+      return (
+        <>
+          <ResponsiveContainer width="100%" height={400}>
+            <BarChart
+              //   width={700}
+              style={{ width: "100%" }}
+              // height={300}
+              data={countries}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+              layout="vertical"
+            >
+              {/* <CartesianGrid strokeDasharray="3 3" /> */}
+              <XAxis type="number" />
+              <YAxis
+                dataKey="country"
+                //   yAxisId={0}
+                //   dataKey={xKey}
+                type="category"
+                axisLine={false}
+                tickLine={false}
+              />
+              <Tooltip />
+              <Legend />
+              {/* <Bar dataKey="pv" fill="#8884d8" /> */}
+              <Bar dataKey="count" fill="#f97316" />
+            </BarChart>
+          </ResponsiveContainer>
+        </>
+      );
+    } else if (location === "City") {
+      return (
+        <>
+          <ResponsiveContainer width="100%" height={400}>
+            <BarChart
+              //   width={700}
+              style={{ width: "100%" }}
+              // height={300}
+              data={cities}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+              layout="vertical"
+            >
+              {/* <CartesianGrid strokeDasharray="3 3" /> */}
+              <XAxis type="number" />
+              <YAxis
+                dataKey="city"
+                //   yAxisId={0}
+                //   dataKey={xKey}
+                type="category"
+                axisLine={false}
+                tickLine={false}
+              />
+              <Tooltip />
+              <Legend />
+              {/* <Bar dataKey="pv" fill="#8884d8" /> */}
+              <Bar dataKey="count" fill="#f97316" />
             </BarChart>
           </ResponsiveContainer>
         </>
@@ -296,91 +230,99 @@ const ProjectAnalytics = () => {
   };
   return (
     <div>
-      <Row justify="space-evenly" align="center">
-        <Col xs={22} md={22}>
-          {/* <DemoColumn /> */}
-          {/* <Bar options={options} data={data} /> */}
-          <ResponsiveContainer width="100%" height={400}>
-            <BarChart
-              //   width={700}
-              style={{ width: "100%" }}
-              height={300}
-              data={data}
-              margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
+      {devices ? (
+        <Row justify="space-evenly" align="center">
+          <Col xs={22} md={22}>
+            {/* <DemoColumn /> */}
+            {/* <Bar options={options} data={data} /> */}
+
+            <Flex>
+              <Title level={5}>{clicks} Clicks</Title>
+            </Flex>
+            <ResponsiveContainer width="100%" height={400}>
+              <BarChart
+                //   width={700}
+                style={{ width: "100%" }}
+                height={300}
+                data={clicksByDate}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                {/* <CartesianGrid strokeDasharray="3 3" /> */}
+                <XAxis dataKey="date" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                {/* <Bar dataKey="pv" fill="#8884d8" /> */}
+                <Bar dataKey="count" fill="#f97316" />
+              </BarChart>
+            </ResponsiveContainer>
+          </Col>
+          <Col md={10} xs={22}>
+            <Card
+              style={{
+                width: "100%",
+                height: "100%",
               }}
             >
-              {/* <CartesianGrid strokeDasharray="3 3" /> */}
-              <XAxis dataKey="date" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              {/* <Bar dataKey="pv" fill="#8884d8" /> */}
-              <Bar dataKey="count" fill="#3aaf61" />
-            </BarChart>
-          </ResponsiveContainer>
-        </Col>
-        <Col md={10} xs={22}>
-          <Card
-            style={{
-              width: "100%",
-              height: "60vh",
-            }}
-          >
-            <Flex
-              justify="space-between"
-              align="start"
-              style={{ width: "100%" }}
-            >
-              <Title level={4} style={{ margin: "0px" }}>
-                Locations
-              </Title>
-              <Segmented
-                defaultValue="Country"
-                color="green"
-                style={{
-                  marginBottom: 8,
-                }}
-                onChange={(value) => setLocation(value)}
-                options={["Country", "City"]}
-              />
-            </Flex>
+              <Flex
+                justify="space-between"
+                align="start"
+                style={{ width: "100%" }}
+              >
+                <Title level={4} style={{ margin: "0px" }}>
+                  Locations
+                </Title>
+                <Segmented
+                  defaultValue="Country"
+                  color="green"
+                  style={{
+                    marginBottom: 8,
+                  }}
+                  onChange={(value) => setLocation(value)}
+                  options={["Country", "City"]}
+                />
+              </Flex>
 
-            {locations()}
-          </Card>
-        </Col>
-        <Col md={10} xs={22}>
-          <Card
-            style={{
-              width: "100%",
-              height: "100%",
-            }}
-          >
-            <Flex
-              justify="space-between"
-              align="start"
-              style={{ width: "100%" }}
+              {locationsChart()}
+            </Card>
+          </Col>
+          <Col md={10} xs={22}>
+            <Card
+              style={{
+                width: "100%",
+                height: "100%",
+              }}
             >
-              <Title level={4} style={{ margin: "0px" }}>
-                Devices
-              </Title>
-              <Segmented
-                defaultValue="Device"
-                color="green"
-                style={{
-                  marginBottom: 8,
-                }}
-                onChange={(value) => setDevicePlatform(value)}
-                options={["Device", "Browser", "Os"]}
-              />
-            </Flex>
-            {devices()}
-          </Card>
-        </Col>
-      </Row>
+              <Flex
+                justify="space-between"
+                align="start"
+                style={{ width: "100%" }}
+              >
+                <Title level={4} style={{ margin: "0px" }}>
+                  Devices
+                </Title>
+                <Segmented
+                  defaultValue="Device"
+                  color="#f97316"
+                  style={{
+                    marginBottom: 8,
+                  }}
+                  onChange={(value) => setDevicePlatform(value)}
+                  options={["Device", "Browser", "Os"]}
+                />
+              </Flex>
+              {devicesChart()}
+            </Card>
+          </Col>
+        </Row>
+      ) : (
+        <h2>Loading...</h2>
+      )}
     </div>
   );
 };
